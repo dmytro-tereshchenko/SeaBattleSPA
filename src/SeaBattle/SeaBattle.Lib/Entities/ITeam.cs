f7741a47885e3e8cb9fa4167ebe746cbdfb70cc8
Dmytro Tereshchenko <dmytro.tereshchenko@nixsolutions.com>
2022-01-19 14:59:31 +0200

    add Entity Team

---
+++ src/SeaBattle/SeaBattle.Lib/Entities/ITeam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Lib.Entities
+{
+    public interface ITeam : IEntity
+    {
+
+        public string Name { get; }
+
+        public uint GameId { get; }
+    }
+}
---
+++ src/SeaBattle/SeaBattle.Lib/Entities/Team.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Lib.Entities
+{
+    public class Team : ITeam
+    {
+       
+        private uint _id;
+
+        private string _name;
+
+        private uint _gameId;
+
+        public uint Id { get => _id; }
+
+        public string Name { get => _name; }
+
+        public uint GameId { get => _gameId; }
+
+        public Team(uint id, string name, uint gameId)
+        {
+            _id = id;
+            _name = name;
+            _gameId = gameId;
+        }
+    }
+}
