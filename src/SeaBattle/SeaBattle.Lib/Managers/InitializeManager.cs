08b5762427a175f39e41efa500fcf152f3626acf
Dmytro Tereshchenko <dmytro.tereshchenko@nixsolutions.com>
2022-01-25 17:11:39 +0200

    Remove xml comments from Initialize manager

--- src/SeaBattle/SeaBattle.Lib/Managers/InitializeManager.cs
+++ src/SeaBattle/SeaBattle.Lib/Managers/InitializeManager.cs
@@ -33,12 +33,6 @@ namespace SeaBattle.Lib.Managers
             _maxNumberOfPlayers = maxNumberOfPlayers;
         }
 
-        /// <summary>
-        /// Creating and getting game field.
-        /// </summary>
-        /// <param name="sizeX">Size X of created game field.</param>
-        /// <param name="sizeY">Size Y of created game field.</param>
-        /// <returns><see cref="IResponseGameField"/> where Value is <see cref="IGameField"/>, State is <see cref="StateCode"/></returns>
         public IResponseGameField CreateGameField(ushort sizeX, ushort sizeY)
         {
             if (sizeX < _minSizeX || sizeX > _maxSizeX || sizeY < _minSizeY || sizeY > _maxSizeY)
@@ -51,11 +45,6 @@ namespace SeaBattle.Lib.Managers
             return new ResponseGameField(field, StateCode.Success);
         }
 
-        /// <summary>
-        /// Method for calculation points which need to purchase ships.
-        /// </summary>
-        /// <param name="field">Field with placement ships on start game - array with type bool, where true - can placed ship, false - wrong cell.</param>
-        /// <returns>Amount of points</returns>
         public int CalculateStartPoints(bool[,] field)
         {
             //Find maximum count ships with minimum size which we can place on the start field.
@@ -134,14 +123,6 @@ namespace SeaBattle.Lib.Managers
             return true;
         }
 
-        /// <summary>
-        /// Method for generating a collection of fields with labels for possible placing ships on start field for teams.
-        /// </summary>
-        /// <param name="gameField">Field of the game</param>
-        /// <param name="numberOfPlayers">Amount of players</param>
-        /// <returns>Collection of fields with ships - arrays with type bool, where true - placed boat, false - empty</returns>
-        /// <exception cref="ArgumentOutOfRangeException"><paramref name="numberOfPlayers"/> out of range</exception>
-        /// <exception cref="ArgumentNullException">There is no gaming field for the given <paramref name="gameField"/>.</exception>
         public ICollection<bool[,]> GenerateStartFields(IGameField gameField, byte numberOfPlayers)
         {
             if (numberOfPlayers > _maxNumberOfPlayers || numberOfPlayers <= 0)
@@ -205,19 +186,9 @@ namespace SeaBattle.Lib.Managers
             return fields;
         }
 
-        /// <summary>
-        /// Getting border size of the game field.
-        /// </summary>
-        /// <returns><see cref="ILimitSize"/></returns>
         public LimitSize GetLimitSizeField() =>
             new LimitSize(_maxSizeX, _maxSizeY, _minSizeX, _minSizeY);
 
-        /// <summary>
-        /// Create and add player to the game
-        /// </summary>
-        /// <param name="game">Current game</param>
-        /// <param name="playerName">Player's name</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode AddPlayerToGame(IGame game, string playerName)
         {
             if (game == null || string.IsNullOrWhiteSpace(playerName))
@@ -235,12 +206,6 @@ namespace SeaBattle.Lib.Managers
             return StateCode.Success;
         }
 
-        /// <summary>
-        /// Get start field by player and game. In case absence of starting fields, create them.
-        /// </summary>
-        /// <param name="game">Current game</param>
-        /// <param name="gamePlayer">Current player</param>
-        /// <returns><see cref="IStartField"/> otherwise null</returns>
         public IStartField GetStartField(IGame game, IGamePlayer gamePlayer)
         {
             if (game == null || gamePlayer == null)
@@ -302,12 +267,6 @@ namespace SeaBattle.Lib.Managers
             return startField;
         }
 
-        /// <summary>
-        /// Create <see cref="IGame"/> by <paramref name="numberOfPlayers"/>
-        /// </summary>
-        /// <param name="numberOfPlayers">Number of players in the game</param>
-        /// <returns><see cref="IGame"/> Created game</returns>
-        /// <exception cref="ArgumentOutOfRangeException">A number of teams are out of possible values.</exception>
         public IGame CreateGame(byte numberOfPlayers)
         {
             if (numberOfPlayers < 1 || numberOfPlayers > _maxNumberOfPlayers)
