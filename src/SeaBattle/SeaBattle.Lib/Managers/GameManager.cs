880e3ca9c055a9d937e343a1305a2acf98b7e0ec
Dmytro Tereshchenko <dmytro.tereshchenko@nixsolutions.com>
2022-01-27 19:53:57 +0200

    Add IGameManager Interface

--- src/SeaBattle/SeaBattle.Lib/Managers/GameManager.cs
+++ src/SeaBattle/SeaBattle.Lib/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using SeaBattle.Lib.Entities;
 using SeaBattle.Lib.Infrastructure;
@@ -54,11 +55,8 @@ namespace SeaBattle.Lib.Managers
         /// <param name="factory">Abstract factory for initializing</param>
         public GameManager(IAbstractGameFactory factory, IGame game) : this(factory) => _game = game;
 
-        /// <summary>
-        /// Create <see cref="IGame"/>
-        /// </summary>
-        /// <param name="numberOfPlayers">Number of players in <see cref="IGame"/></param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
+        #region Create and get
+
         public StateCode CreateGame(byte numberOfPlayers)
         {
             if (_game != null)
@@ -78,12 +76,6 @@ namespace SeaBattle.Lib.Managers
             return StateCode.Success;
         }
 
-        /// <summary>
-        /// Create <see cref="IGameField"/>
-        /// </summary>
-        /// <param name="sizeX">SizeX of <see cref="IGameField"/></param>
-        /// <param name="sizeY">SizeY of <see cref="IGameField"/></param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode CreateGameField(ushort sizeX, ushort sizeY)
         {
             if (_game == null || _game.Field != null)
@@ -101,73 +93,46 @@ namespace SeaBattle.Lib.Managers
             return response.State;
         }
 
-        /// <summary>
-        /// Add player to the game
-        /// </summary>
-        /// <param name="playerName">Player's name</param>
-        /// <returns><see cref="IResponseGamePlayer"/></returns>
         public IResponseGamePlayer AddGamePlayer(string playerName) =>
             _initializeManager.AddPlayerToGame(_game, playerName);
 
-        /// <summary>
-        /// Get start field by player and game. In case absence of starting fields, create them.
-        /// </summary>
-        /// <param name="player">The player who request start field</param>
-        /// <returns><see cref="IResponseStartField"/></returns>
         public IResponseStartField GetStartField(IGamePlayer player) => _initializeManager.GetStartField(_game, player);
 
-        /// <summary>
-        /// Get actual game field
-        /// </summary>
-        /// <param name="player">The player who request game field</param>
-        /// <returns><see cref="IResponseGameField"/></returns>
         public IResponseGameField GetGameField(IGamePlayer player) => _actionManager.GetGameField(player, _game);
 
-        /// <summary>
-        /// Getting a collection of ships, which players can buy by points.
-        /// </summary>
-        /// <returns>Collection of <see cref="ICommonShip"/>, which players can buy by points</returns>
         public ICollection<ICommonShip> GetShips() => _shipManager.GetShips();
 
-        /// <summary>
-        /// Getting a collection of weapons, which players can equip on the ship.
-        /// </summary>
-        /// <returns>Collection of <see cref="IWeapon"/>, which players can equip on the ship</returns>
         public ICollection<IWeapon> GetWeapons() => _shipManager.GetWeapons();
 
-        /// <summary>
-        /// Getting a collection of repairs, which players can equip on the ship.
-        /// </summary>
-        /// <returns>Collection of <see cref="IRepair"/>, which players can equip on the ship</returns>
         public ICollection<IRepair> GetRepairs() => _shipManager.GetRepairs();
 
-        /// <summary>
-        /// Get sorted collection of the ship by distance to the center of the field.
-        /// </summary>
-        /// <param name="player">Current player for filtering <see cref="IGameShip"/>, if null - get all ships without filter.</param>
-        /// <returns><see cref="IDictionary{TKey,TValue}"/> whose generic key argument is <see cref="IGameShip"/>, generic type argument
-        /// is <see cref="ICollection{T}"/> whose generic type argument is (<see cref="ushort"/>, <see cref="ushort"/>) coordinates (X,Y)</returns>
-        IDictionary<IGameShip, ICollection<(ushort, ushort)>> GetSortedShipsFromField(IGamePlayer player = null) =>
+        public IDictionary<IGameShip, ICollection<(ushort, ushort)>> GetSortedShipsFromField(IGamePlayer player = null) =>
             _actionManager.GetFieldWithShips(_game.Field, player);
 
-        /// <summary>
-        /// Get player who win the game;
-        /// </summary>
-        /// <returns><see cref="IGamePlayer"/>, otherwise null</returns>
+        public ICollection<IGameShip> GetVisibletargetsforShip(IGamePlayer player, IGameShip ship, ActionType action)
+        {
+            ICollection<IGameShip> result;
+
+            try
+            {
+                result = _actionManager.GetVisibleTargetsForShip(player, ship, _game.Field, action);
+            }
+            catch (Exception ex)
+            {
+                result = new List<IGameShip>();
+            }
+
+            return result;
+        }
+
         public IGamePlayer GetResultGame() => _game.Winner;
 
-        /// <summary>
-        /// Getting border size of the game field.
-        /// </summary>
-        /// <returns><see cref="LimitSize"/></returns>
         public LimitSize GetLimitSizeField() => _initializeManager.GetLimitSizeField();
 
-        /// <summary>
-        /// Buy ship and save in <see cref="IStartField"/>
-        /// </summary>
-        /// <param name="player">The player who request buying ship</param>
-        /// <param name="ship">The base ship that is buying</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
+        #endregion
+
+        #region Update ship
+
         public StateCode BuyShip(IGamePlayer player, ICommonShip ship)
         {
             if (_game.State == GameState.Finished)
@@ -182,12 +147,6 @@ namespace SeaBattle.Lib.Managers
                 : _shipManager.BuyShip(_game.Players, _shipManager.GetNewShip(player, ship), response.Value);
         }
 
-        /// <summary>
-        /// Sell ship and save in <see cref="IStartField"/>
-        /// </summary>
-        /// <param name="player">The player who request selling ship</param>
-        /// <param name="ship">The ship that is selling</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode SellShip(IGamePlayer player, IGameShip ship)
         {
             if (_game.State == GameState.Finished)
@@ -202,13 +161,6 @@ namespace SeaBattle.Lib.Managers
                 : _shipManager.SellShip(_game.Players, ship, response.Value);
         }
 
-        /// <summary>
-        /// Add weapon to game ship.
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Game ship which adds a weapon.</param>
-        /// <param name="weapon">A weapon (<see cref="IWeapon"/>) which adds.</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode AddWeapon(IGamePlayer player, IGameShip ship, IWeapon weapon)
         {
             if (_game.State == GameState.Finished)
@@ -219,13 +171,6 @@ namespace SeaBattle.Lib.Managers
             return _shipManager.AddWeapon(player, ship, weapon);
         }
 
-        /// <summary>
-        /// Add repair to game ship.
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Game ship which adds a repair.</param>
-        /// <param name="repair">A repair (<see cref="IRepair"/>) which adds.</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode AddRepair(IGamePlayer player, IGameShip ship, IRepair repair)
         {
             if (_game.State == GameState.Finished)
@@ -236,13 +181,6 @@ namespace SeaBattle.Lib.Managers
             return _shipManager.AddRepair(player, ship, repair);
         }
 
-        /// <summary>
-        /// Remove weapon from game ship.
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Game ship which removes a weapon.</param>
-        /// <param name="weapon">Weapon (<see cref="IWeapon"/>) which removes.</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode RemoveWeapon(IGamePlayer player, IGameShip ship, IWeapon weapon)
         {
             if (_game.State == GameState.Finished)
@@ -253,13 +191,6 @@ namespace SeaBattle.Lib.Managers
             return _shipManager.RemoveWeapon(player, ship, weapon);
         }
 
-        /// <summary>
-        /// Remove repair from game ship.
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Game ship which removes a repair.</param>
-        /// <param name="repair">Repair (<see cref="IRepair"/>) which removes.</param>
-        /// <returns><see cref="StateCode"/> is result of operation</returns>
         public StateCode RemoveRepair(IGamePlayer player, IGameShip ship, IRepair repair)
         {
             if (_game.State == GameState.Finished)
@@ -270,15 +201,10 @@ namespace SeaBattle.Lib.Managers
             return _shipManager.RemoveRepair(player, ship, repair);
         }
 
-        /// <summary>
-        /// Place ship on game field
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Target ship</param>
-        /// <param name="posX">X coordinate of the ship's stern</param>
-        /// <param name="posY">Y coordinate of the ship's stern</param>
-        /// <param name="direction">The direction of placement ship</param>
-        /// <returns><see cref="StateCode"/> result of operation</returns>
+        #endregion
+
+        #region Actions
+
         public StateCode PutShipOnField(IGamePlayer player, IGameShip ship, ushort posX, ushort posY,
             DirectionOfShipPosition direction)
         {
@@ -299,13 +225,6 @@ namespace SeaBattle.Lib.Managers
                 : _actionManager.MoveShip(player, ship, posX, posY, direction, _game.Field);
         }
 
-        /// <summary>
-        /// Remove <see cref="IGameShip"/> from <see cref="IGameField"/> to collection in <see cref="IStartField.Ships"/>
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="posX">X coordinate of removed ship</param>
-        /// <param name="posY">Y coordinate of removed ship</param>
-        /// <returns><see cref="StateCode"/> result of operation</returns>
         public StateCode RemoveShipFromField(IGamePlayer player, ushort posX, ushort posY)
         {
             if (_game.State == GameState.Finished)
@@ -320,11 +239,6 @@ namespace SeaBattle.Lib.Managers
                 : _actionManager.TransferShipFromGameField(player, posX, posY, response.Value);
         }
 
-        /// <summary>
-        /// Set state ready for player (finished buying ships and placing them on the game field)
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <returns><see cref="StateCode"/> result of operation</returns>
         public StateCode ReadyPlayer(IGamePlayer player)
         {
             if (_game.State == GameState.Finished)
@@ -355,37 +269,6 @@ namespace SeaBattle.Lib.Managers
             return StateCode.Success;
         }
 
-        /// <summary>
-        /// Get <see cref="ICollection{T}"/> of <see cref="IGameShip"/> on distance of action.
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Current ship</param>
-        /// <param name="action">Type of possible actions (<see cref="ActionType.Attack"/>, <see cref="ActionType.Repair"/>)</param>
-        /// <returns><see cref="ICollection{T}"/> whose generic type argument is <see cref="IGameShip"/>, otherwise default</returns>
-        public ICollection<IGameShip> GetVisibletargetsforShip(IGamePlayer player, IGameShip ship, ActionType action)
-        {
-            ICollection<IGameShip> result;
-
-            try
-            {
-                result = _actionManager.GetVisibleTargetsForShip(player, ship, _game.Field, action);
-            }
-            catch (Exception ex)
-            {
-                result = new List<IGameShip>();
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Attack cell by ship
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Current ship</param>
-        /// <param name="posX">X coordinate of target cell</param>
-        /// <param name="posY">Y coordinate of target cell</param>
-        /// <returns><see cref="StateCode"/> result of operation</returns>
         public StateCode AttackShip(IGamePlayer player, IGameShip ship, ushort posX, ushort posY)
         {
             if (_game.State == GameState.Finished)
@@ -397,14 +280,6 @@ namespace SeaBattle.Lib.Managers
             return CheckEndGame() ? StateCode.GameFinished : state;
         }
 
-        /// <summary>
-        /// Repair ship by cell
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Current ship</param>
-        /// <param name="posX">X coordinate of target cell</param>
-        /// <param name="posY">Y coordinate of target cell</param>
-        /// <returns><see cref="StateCode"/> result of operation</returns>
         public StateCode RepairShip(IGamePlayer player, IGameShip ship, ushort posX, ushort posY)
         {
             if (_game.State == GameState.Finished)
@@ -415,12 +290,6 @@ namespace SeaBattle.Lib.Managers
             return _actionManager.RepairShip(player, ship, posX, posY, _game.Field);
         }
 
-        /// <summary>
-        /// Repair all friendly ship on distance
-        /// </summary>
-        /// <param name="player">Current player</param>
-        /// <param name="ship">Current ship</param>
-        /// <returns><see cref="StateCode"/> result of operation</returns>
         public StateCode RepairAllShip(IGamePlayer player, IGameShip ship)
         {
             if (_game.State == GameState.Finished)
@@ -430,6 +299,10 @@ namespace SeaBattle.Lib.Managers
 
             return _actionManager.RepairAllShip(player, ship, _game.Field);
         }
+
+        #endregion
+
+        #region Services
 
         /// <summary>
         /// Change state <see cref="IGame"/> to next move
@@ -479,5 +352,8 @@ namespace SeaBattle.Lib.Managers
 
             return true;
         }
+
+#endregion
+
     }
 }
