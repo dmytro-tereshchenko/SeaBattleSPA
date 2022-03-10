86e438f730ad432e3d5d5660919b1d5b3c3e27b4
legat4life <legat4life@gmail.com>
2022-03-10 21:15:00 +0200

    add get ships, weapons, repairs to InitializeGameServise in SPA UI

--- src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/services/initialize-game.service.ts
+++ src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/services/initialize-game.service.ts
@@ -5,6 +5,9 @@ import { ErrorLogService } from '../services/error-log.service';
 import { GameSizeLimit } from '../data/game-size-limit';
 import { Game } from '../data/game';
 import { GameSearch } from '../data/game-search';
+import { Ship } from '../data/ship';
+import { Weapon } from '../data/weapon';
+import { Repair } from '../data/repair';
 
 @Injectable({
   providedIn: 'root'
@@ -15,16 +18,37 @@ export class InitializeGameService {
 
   gameSizeUrl: string = 'game/GetLimits';
   gameSearch: string = 'game/GetSearch';
+  shoppingShips: string = 'ship/GetShoppingList';
+  weapons: string = 'ship/GetWeapons';
+  repairs: string = 'ship/GetRepairs';
 
   public GetGameSize(): Observable<GameSizeLimit> {
     return this.dataApi.GetData<GameSizeLimit>(this.gameSizeUrl)
-    .pipe(
-      catchError(this.errorLog.handleError<GameSizeLimit>('GetGameSize')));
+      .pipe(
+        catchError(this.errorLog.handleError<GameSizeLimit>('GetGameSize')));
   }
 
   public GetSearch(): Observable<GameSearch[]> {
     return this.dataApi.GetData<GameSearch[]>(this.gameSearch)
-    .pipe(
-      catchError(this.errorLog.handleError<GameSearch[]>('GetSearch')));
+      .pipe(
+        catchError(this.errorLog.handleError<GameSearch[]>('GetSearch')));
+  }
+
+  public GetShips(): Observable<Ship[]> {
+    return this.dataApi.GetData<Ship[]>(this.shoppingShips)
+      .pipe(
+        catchError(this.errorLog.handleError<Ship[]>('GetShips')));
+  }
+
+  public GetWeapons(): Observable<Weapon[]> {
+    return this.dataApi.GetData<Weapon[]>(this.weapons)
+      .pipe(
+        catchError(this.errorLog.handleError<Weapon[]>('GetWeapons')));
+  }
+
+  public GetRepairs(): Observable<Repair[]> {
+    return this.dataApi.GetData<Repair[]>(this.repairs)
+      .pipe(
+        catchError(this.errorLog.handleError<Repair[]>('GetRepairs')));
   }
 }
