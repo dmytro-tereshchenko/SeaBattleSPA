1e2617368fd37b763467295f459925d48d3f8db4
legat4life <legat4life@gmail.com>
2022-03-21 22:25:46 +0200

    Add game field to game pprocess page

--- src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/components/game-process/game-process.component.ts
+++ src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/components/game-process/game-process.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GameField } from '../../data/game-field';
+import { GameFieldCell } from '../../data/game-field-cell';
+import { Game } from '../../data/game';
+import { DataGameService } from '../../services/data-game.service';
+import { DataGameFieldService } from '../../services/data-game-field.service';
 
 @Component({
   selector: 'app-game-process',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameProcessComponent implements OnInit {
 
-  constructor() { }
+  game: Game;
+  gameField: GameField;
+  labels: boolean[][];
+  selectedShipId: number | null;
+  gameFieldHeight: string;
+  clickCell: GameFieldCell | null;
+
+  constructor(private gameFieldService: DataGameFieldService,
+    private gameService: DataGameService) {
+    this.gameFieldHeight = "80vh";
+    this.clickCell = null;
+    this.selectedShipId = null;
+  }
 
   ngOnInit(): void {
+    this.gameFieldService.getGameField().subscribe(f => this.gameField = f);
+    this.gameService.getGame().subscribe(g => this.game = g);
+  }
+
+  onNotifyGameFieldClick(cell: GameFieldCell) {
+
+  }
+
+  onNotifyGameFieldDblClick(cell: GameFieldCell) {
+
   }
 
 }
