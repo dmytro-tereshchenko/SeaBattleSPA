be0806a98e54474a2aebebf9ba3553798dd4058d
legat4life <legat4life@gmail.com>
2022-03-22 21:48:58 +0200

    Add convert object to object of class GameShip in UI

--- src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/data/game-ship.ts
+++ src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/data/game-ship.ts
@@ -28,21 +28,23 @@ export class GameShip implements IGameShip {
     weapons: Weapon[];
     repairs: Repair[];
 
-    constructor() { }
+    constructor(payload: Partial<GameShip>) {
+        this.id = payload.id || 0
+        this.hp = payload.hp || 0
+        this.shipType = payload.shipType || 0
+        this.size = payload.size || 0
+        this.maxHp = payload.maxHp || 0
+        this.speed = payload.speed || 0
+        this.gamePlayerId = payload.gamePlayerId || 0
+        this.weapons = payload.weapons || []
+        this.repairs = payload.repairs || []
+    };
 
-    attackRange(): number {
-        return this.weapons.reduce((acc, shot) => acc = acc > shot.attackRange ? acc : shot.attackRange, 0);
-    }
+    attackRange = (): number => this.weapons.reduce((acc, shot) => acc = acc > shot.attackRange ? acc : shot.attackRange, 0);
 
-    repairRange(): number {
-        return this.repairs.reduce((acc, shot) => acc = acc > shot.repairRange ? acc : shot.repairRange, 0);
-    }
+    repairRange = (): number => this.repairs.reduce((acc, shot) => acc = acc > shot.repairRange ? acc : shot.repairRange, 0);
 
-    damage(): number {
-        return this.weapons.reduce((acc, shot) => acc = acc + shot.damage, 0);
-    }
+    damage = (): number => this.weapons.reduce((acc, shot) => acc = acc + shot.damage, 0);
 
-    repairPower(): number {
-        return this.repairs.reduce((acc, shot) => acc = acc + shot.repairPower, 0);
-    }
+    repairPower = (): number => this.repairs.reduce((acc, shot) => acc = acc + shot.repairPower, 0);
 }
