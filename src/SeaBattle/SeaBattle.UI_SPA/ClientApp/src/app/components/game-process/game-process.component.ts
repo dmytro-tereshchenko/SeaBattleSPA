75ed5f8012d3cffbd984d6669d78fd4ca1b3738f
legat4life <legat4life@gmail.com>
2022-03-23 22:09:34 +0200

    Add RepairAllShips and EnDMove to GameProcess page in UI

--- src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/components/game-process/game-process.component.ts
+++ src/SeaBattle/SeaBattle.UI_SPA/ClientApp/src/app/components/game-process/game-process.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { GameField } from '../../data/game-field';
 import { GameFieldCell } from '../../data/game-field-cell';
 import { Game } from '../../data/game';
+import { Player } from '../../data/player';
 import { colorPlayers } from '../../data/mock-color-players';
 import { DataGameService } from '../../services/data-game.service';
 import { DataGameFieldService } from '../../services/data-game-field.service';
+import { DataShipService } from '../../services/data-ship.service';
+import { ShipActionService } from '../../services/ship-action.service';
 import { AuthService } from '../../core/services/auth.service';
+import { ActionType } from 'src/app/data/action-type';
 
 @Component({
   selector: 'app-game-process',
@@ -22,25 +27,37 @@ export class GameProcessComponent implements OnInit {
   clickCell: GameFieldCell | null;
   private colorShips: string[] = colorPlayers;
   private players: string[];
+  private player: Player | undefined;
+  private isMoved: boolean;
+  private isActioned: boolean;
+  private updateTimeOut: number = 1000;
+  private subscription: Subscription;
+  private source: Observable<number>
 
   constructor(private gameFieldService: DataGameFieldService,
     private gameService: DataGameService,
-    private userService: AuthService) {
+    private shipService: DataShipService,
+    private userService: AuthService,
+    private actionService: ShipActionService) {
     this.gameFieldHeight = "80vh";
     this.clickCell = null;
     this.selectedShipId = null;
+    this.isMoved = false;
+    this.isActioned = false;
   }
 
   ngOnInit(): void {
+    this.source = interval(this.updateTimeOut);
+
     this.gameFieldService.getGameField().subscribe(f => this.gameField = f);
 
     this.userService.getUser().then(user => this.gameService.getGame().subscribe(game => {
       this.game = game;
       this.players = [];
-      var player = game.players.find(p => p.name === (user?.profile.name ?? ''));
+      this.player = game.players.find(p => p.name === (user?.profile.name ?? ''));
 
-      if (player) {
-        this.players.push(player.name);
+      if (this.player) {
+        this.players.push(this.player.name);
       }
 
       game.players.forEach(p => {
@@ -49,6 +66,10 @@ export class GameProcessComponent implements OnInit {
         }
       })
     }))
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   getColor(playerName: string): string {
@@ -67,7 +88,53 @@ export class GameProcessComponent implements OnInit {
   }
 
   onNotifyGameFieldDblClick(cell: GameFieldCell) {
+    if (this.game.currentPlayerMove == this.player?.name &&
+      cell &&
+      cell.gameShipId &&
+      !this.isActioned &&
+      ((this.selectedShipId == null && cell.gameShipId) || (
+        this.selectedShipId && cell.gameShipId === this.selectedShipId))) {
+      this.actionService.repairAll(cell.gameShipId).subscribe(state => {
+        if (state === 10 || state === 21) {
+          this.isActioned = true;
+          this.selectedShipId = cell.gameShipId;
 
+          this.actionService.getVissibleShips(cell.gameShipId!, ActionType.repair).subscribe(ships => {
+            ships.forEach(shipId => this.shipService.getShipFromServer(shipId))
+          })
+
+          this.gameFieldService.getGameFieldFromServer().subscribe(field => this.gameField = field);
+
+          if (this.isMoved) {
+            this.endMove();
+          }
+        }
+      })
+    }
   }
 
+  endMove() {
+    this.isActioned = false;
+    this.isMoved = false;
+    this.selectedShipId = null;
+
+    this.actionService.endMove().subscribe(state => {
+      if (state === 10) {
+        this.gameService.getGameFromServer().subscribe(game => {
+          this.game = game;
+          this.subscription = this.source.subscribe(val => this.update());
+        })
+      }
+    })
+  }
+
+  private update() {
+    this.gameService.getGameFromServer().subscribe(game => {
+      if (game.currentPlayerMove !== this.game.currentPlayerMove) {
+        this.game = game;
+        this.gameFieldService.getGameFieldFromServer().subscribe(field => this.gameField = field);
+        this.subscription.unsubscribe();
+      }
+    })
+  }
 }
